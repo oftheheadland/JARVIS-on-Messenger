ce50a275ec2d9b98c6f383d4d056c3448290e60d
Andrew VanNess <avanness@student.bridgew.edu>
2018-04-13 11:01:55 -0400

    fixed bug with brazil time zone

--- modules/src/time.py
+++ modules/src/time.py
@@ -1,6 +1,8 @@
 import os
 from datetime import datetime
 
+from pytz import timezone
+
 import requests
 
 import config
@@ -13,10 +15,33 @@ TIME_ZONE_DB_API_KEY = os.environ.get('TIME_ZONE_DB_API_KEY', config.TIME_ZONE_D
 def process(input, entities):
     output = {}
     try:
+        print(input)
+        print(entities)
         r = requests.get(
             'http://open.mapquestapi.com/nominatim/v1/search.php?key=' + MAPQUEST_CONSUMER_KEY + '&format=json&q=' +
             entities['time_location'][0]['value'] + '&limit=1')
         location_data = r.json()
+
+        if "brazil" in input:
+
+            print(entities['time_location'][0]['value'])
+            #south_africa = timezone('Brazil/Acre')
+            #sa_time = datetime.now(south_africa)
+            #print(sa_time.strftime('%Y-%m-%d_%H-%M-%S'))
+
+            location = timezone('Brazil/Acre')
+            local_time = datetime.now(location)
+            print(local_time.strftime('%Y-%m-%d_%H-%M-%S'))
+
+            output['input'] = input
+            output['output'] = TextTemplate(
+                'Location: ' + "Brazil" + '\nTime: ' + local_time.strftime('%Y-%m-%d_%H-%M-%S')).get_message()
+            output['success'] = True
+            return output
+
+
+
+
         r = requests.get('http://api.timezonedb.com/?lat=' + location_data[0]['lat'] + '&lng=' + location_data[0][
             'lon'] + '&format=json&key=' + TIME_ZONE_DB_API_KEY)
         time_data = r.json()
