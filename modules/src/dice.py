e10db0ba195d97a47d86192e9dec72ad58d30a62
Nick <nroppolo@student.bridgew.edu>
2018-04-19 11:11:32 -0400

    added gif to output set to see if it works. left type as image

--- modules/src/dice.py
+++ modules/src/dice.py
@@ -5,12 +5,12 @@ from templates.attachment import AttachmentTemplate
 from templates.quick_replies import add_quick_reply
 
 dice_sides = {
-    1: 'http://fa2png.io/media/icons/foundation-icon-fonts/2015-02-16/die-one/256/0/e74c3c_none.png',
-    2: 'http://fa2png.io/media/icons/foundation-icon-fonts/2015-02-16/die-two/256/0/e74c3c_none.png',
-    3: 'http://fa2png.io/media/icons/foundation-icon-fonts/2015-02-16/die-three/256/0/e74c3c_none.png',
-    4: 'http://fa2png.io/media/icons/foundation-icon-fonts/2015-02-16/die-four/256/0/e74c3c_none.png',
-    5: 'http://fa2png.io/media/icons/foundation-icon-fonts/2015-02-16/die-five/256/0/e74c3c_none.png',
-    6: 'http://fa2png.io/media/icons/foundation-icon-fonts/2015-02-16/die-six/256/0/e74c3c_none.png'
+    1: 'https://gfycat.com/UnconsciousLegalDoe',
+    2: 'https://gfycat.com/UnconsciousLegalDoe',
+    3: 'https://gfycat.com/UnconsciousLegalDoe',
+    4: 'https://gfycat.com/UnconsciousLegalDoe',
+    5: 'https://gfycat.com/UnconsciousLegalDoe',
+    6: 'https://gfycat.com/UnconsciousLegalDoe',
 }
 
 
