6bfeaa0b57ce2bedd0e5e5c9c638b120d9510fb3
Nick <nroppolo@student.bridgew.edu>
2018-04-19 11:09:53 -0400

    reverted all changes to make a stable build

--- modules/src/dice.py
+++ modules/src/dice.py
@@ -4,7 +4,7 @@ import modules
 from templates.attachment import AttachmentTemplate
 from templates.quick_replies import add_quick_reply
 
-dice_sidesAnimate = {
+dice_sides = {
     1: 'http://fa2png.io/media/icons/foundation-icon-fonts/2015-02-16/die-one/256/0/e74c3c_none.png',
     2: 'http://fa2png.io/media/icons/foundation-icon-fonts/2015-02-16/die-two/256/0/e74c3c_none.png',
     3: 'http://fa2png.io/media/icons/foundation-icon-fonts/2015-02-16/die-three/256/0/e74c3c_none.png',
@@ -12,18 +12,10 @@ dice_sidesAnimate = {
     5: 'http://fa2png.io/media/icons/foundation-icon-fonts/2015-02-16/die-five/256/0/e74c3c_none.png',
     6: 'http://fa2png.io/media/icons/foundation-icon-fonts/2015-02-16/die-six/256/0/e74c3c_none.png'
 }
-dice_sides = {
-    1: 'https://gfycat.com/UnconsciousLegalDoe',
-    2: 'https://gfycat.com/UnconsciousLegalDoe',
-    3: 'https://gfycat.com/UnconsciousLegalDoe',
-    4: 'https://gfycat.com/UnconsciousLegalDoe',
-    5: 'https://gfycat.com/UnconsciousLegalDoe',
-    6: 'https://gfycat.com/UnconsciousLegalDoe',
-}
+
 
 def process(input, entities=None):
-    #message = AttachmentTemplate(dice_sidesAnimate[random.randint(1, 6)], type='image').get_message()
-    message = AttachmentTemplate(dice_sides[random.randint(1, 6)], type='gif').get_message(),
+    message = AttachmentTemplate(dice_sides[random.randint(1, 6)], type='image').get_message()
     message = add_quick_reply(message, 'Roll again!', modules.generate_postback('dice'))
     message = add_quick_reply(message, 'Flip a coin.', modules.generate_postback('coin'))
     output = {
