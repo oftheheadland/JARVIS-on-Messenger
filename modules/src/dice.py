121abac406b1312bd4555b8a2c72c29b3d9813ae
Nick <nroppolo@student.bridgew.edu>
2018-04-24 12:29:01 -0400

    added all 6 animations

--- modules/src/dice.py
+++ modules/src/dice.py
@@ -5,12 +5,12 @@ from templates.attachment import AttachmentTemplate
 from templates.quick_replies import add_quick_reply
 
 dice_sides = {
-    1: 'https://thumbs.gfycat.com/SeriousPiercingBangeltiger-size_restricted.gif',
-    2: 'https://thumbs.gfycat.com/SeriousPiercingBangeltiger-size_restricted.gif',
+    1: 'https://thumbs.gfycat.com/NewFoolhardyAvocet-size_restricted.gif',
+    2: 'https://thumbs.gfycat.com/ShyCelebratedGraywolf-size_restricted.gif',
     3: 'https://thumbs.gfycat.com/SeriousPiercingBangeltiger-size_restricted.gif',
-    4: 'https://thumbs.gfycat.com/SeriousPiercingBangeltiger-size_restricted.gif',
-    5: 'https://thumbs.gfycat.com/SeriousPiercingBangeltiger-size_restricted.gif',
-    6: 'https://thumbs.gfycat.com/SeriousPiercingBangeltiger-size_restricted.gif',
+    4: 'https://thumbs.gfycat.com/AdoredFirmLcont-size_restricted.gif',
+    5: 'https://thumbs.gfycat.com/FrequentQueasyEgg-size_restricted.gif',
+    6: 'https://thumbs.gfycat.com/WanTameAfricanparadiseflycatcher-size_restricted.gif',
 }
 
 
