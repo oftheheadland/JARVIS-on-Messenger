05b7921be6a09ba6f072a58281a8db74521961a0
Stephen MacLellan <smaclellan@student.bridgew.edu>
2018-04-25 13:02:16 -0400

    changes the quotes module to parse brainyquotes rss feed

--- modules/src/quote.py
+++ modules/src/quote.py
@@ -16,25 +16,26 @@ def process(input, entities=None):
         # request to dowmload website content
         r = requests.get(url)
         soup = BeautifulSoup(r.content, 'html.parser')
-        contentVar = soup.currentTag
-        titleVar = contentVar.findAll('title')
-        descVar = contentVar.findAll('description')
+        # contentVar = soup.currentTag
+        # titleVar = soup.findAll('title')
+        # descVar = soup.findAll('description')
 
-        titleQu = titleVar[2]
-        descQu = descVar[2]
-
-        quoteDay = titleQu + " : " + descQu
+        title_tag = soup.findAll("title")[2]
+        description_tag = soup.findAll("description")[2]
+        # print(title_tag.text)
+        # print(description_tag.text)
+        quotes = (title_tag.text + ' ; ' + description_tag.text)
 
 
         #with open(config.QUOTES_SOURCE_FILE) as quotes_file:
             #quotes = json.load(quotes_file)
             #quotes_list = quotes['quotes']
-        #message = TextTemplate(choice(quoteDay)).get_message()
+        message = TextTemplate(choice(quotes)).get_message()
             #message = add_quick_reply(message, 'Another one!', modules.generate_postback('quote'))
-        #message = add_quick_reply(message, 'Show me a fact.', modules.generate_postback('fact'))
-        #message = add_quick_reply(message, 'Tell me a joke.', modules.generate_postback('joke'))
+        message = add_quick_reply(message, 'Show me a fact.', modules.generate_postback('fact'))
+        message = add_quick_reply(message, 'Tell me a joke.', modules.generate_postback('joke'))
         output['input'] = input
-        output['output'] = TextTemplate(choice(quoteDay)).get_message()
+        output['output'] = message
         output['success'] = True
     except:
         output['success'] = False
