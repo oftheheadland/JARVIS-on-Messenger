33d6c8e1da64e7b8c852b4535664da4ae40833ac
Andrew VanNess <avanness@student.bridgew.edu>
2018-04-28 17:20:56 -0400

    added test file for math module

---
+++ modules/tests/test_math.py
@@ -0,0 +1,9 @@
+import modules
+
+
+def test_math():
+    assert ('math' == modules.process_query('math 4*4')[0])
+    assert ('math' == modules.process_query('distance between earth and sun math')[0])
+    assert ('math' == modules.process_query('10 times 10 math')[0])
+    assert ('math' != modules.process_query('what is 5 * 3')[0])
+    assert ('math' != modules.process_query('something random')[0])
